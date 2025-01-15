069e83e96bc463239a7906bea58ae73b4a7b7651
Barak911 <58632451+Barak911@users.noreply.github.com>
2025-01-15 10:53:19 +0200

    Add files via upload

---
+++ scripts/user-data.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+# Define S3 bucket and .env file
+S3_BUCKET_NAME="entytracker-cicd"
+ENV_FILE_NAME=".env"
+
+
+apt-get update
+
+# Install prerequisites
+apt-get install -y ca-certificates curl gnupg git
+
+# Create directory for keyrings
+install -m 0755 -d /etc/apt/keyrings
+
+# Download and install Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up the repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add ubuntu user to docker group to run docker without sudo
+usermod -aG docker ubuntu
+
+systemctl enable docker
+systemctl start docker
+
+mkdir -p /home/ubuntu/workspace
+
+cd /home/ubuntu/workspace
+
+# Clone repository
+git clone https://github.com/Barak911/entryTracker_CICD.git
+
+chown -R ubuntu:ubuntu /home/ubuntu/workspace
+
+# INSTALL AWS CLI AND UNZIP
+cd /home/ubuntu/
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+sudo apt install unzip
+unzip -u awscliv2.zip
+sudo /home/ubuntu/aws/install
+
+# Download .env from S3
+echo "Downloading .env file from S3..."
+aws s3 cp s3://$S3_BUCKET_NAME/$ENV_FILE_NAME /home/ubuntu/workspace/.env
+
+# Verify .env download
+if [ -f "/home/ubuntu/.env" ]; then
+    echo ".env file downloaded successfully to /home/ubuntu/workspace/.env"
+else
+    echo "Failed to download .env file from S3."
+    exit 1
+fi
+
+# Copy and overwrite .env to target directory
+echo "Copying .env to the target directory..."
+mkdir -p /home/ubuntu/workspace/entryTracker_CICD
+cp -f /home/ubuntu/workspace/.env /home/ubuntu/workspace/entryTracker_CICD/.env
+
+# Make the .env file readable
+chmod 600 /home/ubuntu/workspace/entryTracker_CICD/.env
+
